37035769d5c366785be2a6203d612a95f76e0e6b
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-26 12:15:42 -0600

    firt commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,26 @@
+// Lesson - Java I - Introduction To Java
+// Status -Complete
+public class HelloWorld {
+	public static void main (String[] args) {
+		System.out.println("Hello, World!"); // This...
+
+//		System.out.print("Hello, "); //---- is the same as this
+//		System.out.print("World!"); //---- and this
+
+//===== BONUS =====
+	//	1.	Build the project from the command line
+	//
+	//	1.	Open a terminal and navigate to your project.
+	//
+	//	2.	Change directories into the src directory. You should see your HelloWorld.java file here.
+	//
+	//	3.	Run the following command to compile your java file
+	//			javac HelloWorld.java
+
+	//	4.	View the contents of the directory again, you should now see a file named HelloWorld.class, this is the compiled java bytecode.
+	//
+	//	5.	Run your program with the following command
+	//			java HelloWorld
+
+	}
+}
