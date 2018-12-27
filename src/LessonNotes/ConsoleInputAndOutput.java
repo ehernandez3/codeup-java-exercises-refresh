1e5e05140eba37008b724b08c8f74aa00716c63b
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-27 13:14:41 -0600

    added ConsoleInputAndOutput.java

---
+++ src/LessonNotes/ConsoleInputAndOutput.java
@@ -0,0 +1,11 @@
+// Lesson - Java I - Console Input and Output
+package LessonNotes;
+
+public class ConsoleInputAndOutput {
+
+// Printing Data To The Console
+
+	//String name = "codeup";
+	//System.out.format("Hello there, %s. Nice to see you.\n", name);
+
+}
