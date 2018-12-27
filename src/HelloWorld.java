f6f0008cb86abd5e81477336e4151ec9db322be3
Enrique Hernandez <hernandez.x.enrique@gmail.com>
2018-12-27 12:51:40 -0600

    Completed exercises

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,7 +1,7 @@
 // Lesson - Java I - Introduction To Java
 // Status - Complete
 // Lesson - Java I - Syntax, Types and Variables
-// Status -
+// Status - Complete
 
 public class HelloWorld {
 	public static void main (String[] args) {
@@ -83,32 +83,41 @@ public class HelloWorld {
 //
 //		9. Change your code to declare myNumber as a float. Assign the value 3.14 to it.
 // 			What happens? What are two ways we could fix this?
+
+//				float mynumber = 3.14; // Error: java: incompatible types: possible lossy conversion from double to float
+
+//				double myNumber = 3.14; // One way to fix is to change the declared type to double.
+//				float myNewNumber = (float) myNumber; // Another is to cast it to a float type after declaring as a double.
+//				System.out.println(myNewNumber);
 //
 //		10. Copy and paste the following code blocks one at a time and execute them
+// 			What is the difference between the above code blocks? Explain why the code outputs what it does.
 
-		//		int x = 5;
-		//		System.out.println(x++);
-		//		System.out.println(x);
-		//
-		//		int x = 5;
-		//		System.out.println(++x);
-		//		System.out.println(x);
+//				int x = 5;
+//				System.out.println(x++); // ++ sign is after (post increment) the x, which increments the value of x (5) by
+//				System.out.println(x);   // one (6) after it prints.
 
-		// What is the difference between the above code blocks? Explain why the code outputs what it does.
+//				int x = 5;
+//				System.out.println(++x); // Pre increment (++ before the value of x), will increase the value of x and
+//				System.out.println(x);   // then print out he value.
+
 
 //		11. Try to create a variable named class. What happens?
+
+//				int class = 34565; // Not allowed. Will get error.  Reserved word (class) not allowed as variable name.
 //
 //		12. Object is the most generic type in Java. You can assign any value to a variable of type Object.
 // 			What do you think will happen when the following code is run?
 //
-		//		String theNumberThree = "three";
-		//		Object o = theNumberThree;
-		//		int three = (int) o;
+//				String theNumberThree = "three";
+//				Object o = theNumberThree;
+////				int three = (int) o; // Runtime Exception. Syntax is correct but you cannot cast a string type (o), to
+									 // an int type.
 
 //		Copy and paste the code above and then run it. Does the result match with your expectation?
 //		How is the above example different from this code block?
 //
-//		int three = (int) "three";
+//				int three = (int) "three"; // This will give you a Compile time error. Syntax error is caught before compiling.
 
 //		What are the two different types of errors we are observing?
 //
@@ -117,18 +126,49 @@ public class HelloWorld {
 //
 		//		int x = 4;
 		//		x = x + 5;
+
+//							int x = 4;
+//							x += 5; //------------------------------ Shorthand "+="
+//							System.out.println(x);
 		//
 		//		int x = 3;
 		//		int y = 4;
 		//		y = y * x;
+
+//							int x = 3;
+//							int y = 4;
+//							y *= x; //------------------------------ Shorthand "*="
+//							System.out.println(y);
 		//
 		//		int x = 10;
 		//		int y = 2;
 		//		x = x / y;
 		//		y = y - x;
 
+//							int x = 10;
+//							int y = 2;
+//							x /= y; //------------------------------ Shorthand "/="
+//							y -= x; //------------------------------ Shorthand "-="
+//							System.out.println(x);
+//							System.out.println(y);
+
 //		14. What happens if you assign a value to a numerical variable that is larger (or smaller)
 // 			than the type can hold? What happens if you increment a numeric variable past the type's capacity?
+
+				int myNumber = 2147483648; // Error: java: integer number too large: 2147483648
+										   // Max value for integer is 2147483647
+
+				System.out.println(Byte.MIN_VALUE); // -128
+				System.out.println(Byte.MAX_VALUE); // 127
+				System.out.println(Short.MIN_VALUE); // -32768
+				System.out.println(Short.MAX_VALUE); // 32767
+				System.out.println(Integer.MIN_VALUE); // -2147483648
+				System.out.println(Integer.MAX_VALUE); // 2147483647
+				System.out.println(Long.MIN_VALUE); // -9223372036854775808
+				System.out.println(Long.MAX_VALUE); // 9223372036854775807
+				System.out.println(Double.MIN_VALUE); // 4.9E-324
+				System.out.println(Double.MAX_VALUE); // 1.7976931348623157E308
+
 //
 //		Hint: Integer.MAX_VALUE is a class constant (we'll learn more about these later) that holds the maximum value for the int type.
 	}
